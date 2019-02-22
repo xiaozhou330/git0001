384fd19d04fd89c570a9e9b3907a9a49780bfcb4
zhansan <123@qq.com>
2019-02-22 20:52:04 +0800

    小哥到此一游

--- src/cn/itheima/MyTest.java
+++ src/cn/itheima/MyTest.java
@@ -3,6 +3,14 @@ package cn.itheima;
 public class MyTest {
 
     public static void main(String[] args) {
+        System.out.println("qin");
+        System.out.println("xiao");
+        System.out.println("ge");
+        System.out.println("dao");
+        System.out.println("ci");
+        System.out.println("yi");
+        System.out.println("you");
+        System.out.println("!");
 
     }
 }
