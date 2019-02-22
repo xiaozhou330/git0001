22c2b3171326f3eae83d36f0d3a26e10c1a7188f
zhansan <123@qq.com>
2019-02-22 20:58:57 +0800

    小哥到此er游

--- src/cn/itheima/MyTest.java
+++ src/cn/itheima/MyTest.java
@@ -3,6 +3,14 @@ package cn.itheima;
 public class MyTest {
 
     public static void main(String[] args) {
+        System.out.println("asdasdas");
+        System.out.println("asdasdas");
+        System.out.println("asdasdas");
+        System.out.println("asdasdas");
+        System.out.println("asdasdas");
+        System.out.println("asdasdas");
+        System.out.println("asdasdas");
+        System.out.println("asdasdas");
 
     }
 }
