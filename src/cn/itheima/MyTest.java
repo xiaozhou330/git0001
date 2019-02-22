1108d8264691d1b886f3c92dd1a4143401447c6c
admin <admin@itheima.com>
2019-02-22 21:00:24 +0800

    22222222

--- src/cn/itheima/MyTest.java
+++ src/cn/itheima/MyTest.java
@@ -3,6 +3,7 @@ package cn.itheima;
 public class MyTest {
 
     public static void main(String[] args) {
-
+        System.out.println("11111111111");
+        System.out.println("22222222222");
     }
 }
